71242589bd4219cec78b73bef5ddcc2b453c5b9f
chrisargy <chrisargy@hotmail.gr>
2020-12-17 21:17:54 +0200

    added weather function

--- app/src/main/java/com/example/carui/HomeActivity.java
+++ app/src/main/java/com/example/carui/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.carui;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.text.DateFormatSymbols;
+import java.text.DecimalFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -19,6 +21,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     private TextView weatherTextView, timeTextView, dateTextView;
     private ImageButton gpsBtn, radioBtn, phoneBtn, settingsBtn;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +33,21 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         dateTextView = findViewById(R.id.textview_date);
 
         Calendar cal = Calendar.getInstance(Locale.getDefault());
-        timeTextView.setText(cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE));
-        dateTextView.setText(cal.get(Calendar.DAY_OF_MONTH) + " " + (new DateFormatSymbols().getMonths()[cal.get(Calendar.MONTH)]).substring(0, 3).toUpperCase());
+        int month = cal.get(Calendar.MONTH);
+        double temp = (Math.pow((month - 6), 4))/(-25) + 35;
+        DecimalFormat df = new DecimalFormat("##.#");
+        weatherTextView.setText(df.format(temp) + " C");
+        //weatherTextView.setText(month + " C");
+
+        int h = cal.get(Calendar.HOUR_OF_DAY);
+        String hours = h < 10 ? "0"+h : String.valueOf(h);
+        int m = cal.get(Calendar.MINUTE);
+        String minutes = m < 10 ? "0"+m : String.valueOf(m);
+        timeTextView.setText(hours + ":" + minutes);
+
+        int d = cal.get(Calendar.DAY_OF_MONTH);
+        String day = d < 10 ? "0"+d : String.valueOf(d);
+        dateTextView.setText(day + " " + (new DateFormatSymbols().getMonths()[month]).substring(0, 3).toUpperCase());
 
         gpsBtn = findViewById(R.id.home_gps_btn);
         radioBtn = findViewById(R.id.home_radio_btn);
@@ -40,20 +56,17 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    protected void onPause()
-    {
-        super.onPause();
-    }
+    protected void onPause() {super.onPause();}
 
     @Override
     public void onClick(View v) {
         if (v == gpsBtn)
-            startActivity(new Intent(this, GPSActivity.class));
+            startActivity(new Intent(HomeActivity.this, GPSActivity.class));
         else if (v == radioBtn)
-            startActivity(new Intent(this, RadioActivity.class));
+            startActivity(new Intent(HomeActivity.this, RadioActivity.class));
         else if (v == phoneBtn)
-            startActivity(new Intent(this, PhoneActivity.class));
+            startActivity(new Intent(HomeActivity.this, PhoneActivity.class));
         else if (v == settingsBtn)
-            startActivity(new Intent(this, SettingsActivity.class));
+            startActivity(new Intent(HomeActivity.this, SettingsActivity.class));
     }
 }
